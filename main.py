56d2c0f8e7b6309958c7706e846f544b826ff2cd
Your Name <you@example.com>
2023-08-15 17:10:59 -0400

    Inital Startup Only

---
+++ main.py
@@ -0,0 +1,16 @@
+print('It is widely believed that for NBA guards their third season is vital in revealing whether or not they can be a superstar.\n\n'
+      'Here I have compiled a directory of statistics from the first three seasons of several notable current and historical NBA guards.\n\n'
+      'Use this directory to see the statistics of these players. Furthermore, I have provided a tool which can predict the third year stats for current second year guards based on historical trends.\n\n')
+
+while True:
+    prompt = input('What would you like to do?\n1.Look up stats for a current NBA guard\n2.Look up stats for a historical NBA guard\n3.See 3rd year predicted stats for a current 2nd year guard\n4.Exit\n')
+    if prompt == '1':
+        pass
+    elif prompt == '2':
+        pass
+    elif prompt == '3':
+        pass
+    elif prompt == '4':
+        break
+    else:
+        print('Pick a valid numerical response')
